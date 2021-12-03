c486971a50f29ec6e918bbfefe080de0f37d85d5
Vũ Khoan <93373792+khoanvuviefox@users.noreply.github.com>
2021-12-03 09:45:33 +0700

    Thực hành Module 1

---
+++ Bai2/script.js
@@ -0,0 +1,18 @@
+let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let number = parseInt(prompt("Nhập số từ 1 đến 10"));
+document.write(indexOfArrays(number, numberArray));
+
+function indexOfArrays(num, arr) {
+    if (Number.isNaN(num) || num < 1 || num > 10) {
+        return "Xin lỗi! Nhập lại một sớ từ 1 đến 10";
+    } else if (!Array.isArray(arr)) {
+        return "Xin lỗi! Đây không phải mảng";
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === num) {
+                return ("Mảng thứ " + i + " nằm trong mảng " + arr.toString());
+            }
+        }
+    }
+    return false;
+}
